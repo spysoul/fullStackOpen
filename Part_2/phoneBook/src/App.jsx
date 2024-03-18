6cc2a3e7b3c29dad2a68678f83c085cedd9b0b14
spysoul <danielgoal76@gmail.com>
2024-03-18 16:46:03 +0000

    Ejercicios 2.6 y 2.7

---
+++ Part_2/phoneBook/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addNote = (e) => {
+    e.preventDefault();
+    // Check if name already exists
+    let nameExists = persons.find((person) => person.name === newName);
+
+    nameExists
+      ? alert(`${newName} is already added to phonebook`)
+      : setPersons(persons.concat([{ name: newName }]));
+
+    setNewName("");
+  };
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNote}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
