f14ab63c294776231e863ad6c379a3dcc442b4e8
spysoul <danielgoal76@gmail.com>
2024-03-18 20:46:36 +0000

    Exercises 2.8 - 2.10

--- Part_2/phoneBook/src/App.jsx
+++ Part_2/phoneBook/src/App.jsx
@@ -1,40 +1,106 @@
 import { useState } from "react";
 
-const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
+const Filter = ({ filter, handleFilterChange }) => {
+  return (
+    <div>
+      filter shown with <input value={filter} onChange={handleFilterChange} />
+    </div>
+  );
+};
 
-  const addNote = (e) => {
+const PersonForm = ({
+  addPerson,
+  newName,
+  newNumber,
+  handleNameChange,
+  handleNumberChange,
+}) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+const Persons = ({ persons, filter }) => {
+  return persons
+    .filter(
+      (p) =>
+        p.name.toLowerCase().includes(filter.toLowerCase()) ||
+        p.number.toLowerCase().includes(filter.toLowerCase())
+    )
+    .map((person) => (
+      <p key={person.name}>
+        {person.name} {person.number}
+      </p>
+    ));
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const addPerson = (e) => {
     e.preventDefault();
     // Check if name already exists
     let nameExists = persons.find((person) => person.name === newName);
 
-    nameExists
-      ? alert(`${newName} is already added to phonebook`)
-      : setPersons(persons.concat([{ name: newName }]));
+    if (nameExists) {
+      alert(`${newName} is already added to phonebook`);
+    } else if (newName === "" || newNumber === "") {
+      alert("Name and Number are required fields");
+    } else {
+      setPersons(persons.concat([{ name: newName, number: newNumber }]));
+    }
 
     setNewName("");
+    setNewNumber("");
   };
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
   };
 
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  };
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addNote}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+
+      <h2>add a new</h2>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+      />
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
-      ))}
+
+      <Persons persons={persons} filter={filter} />
     </div>
   );
 };
