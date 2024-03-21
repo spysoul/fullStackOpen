c562126079cd01eb47f8f3111e0ed932bf77b865
spysoul <danielgoal76@gmail.com>
2024-03-21 10:28:52 +0000

    remove some data

--- Part_2/countries/src/services/weather.js
+++ Part_2/countries/src/services/weather.js
@@ -1,12 +1,8 @@
 import axios from "axios";
 
 // APIKEY
-// const api_key = "9WQCPNN5TRJT9U6A6NR8TMFT8";
 const api_key = import.meta.env.VITE_WEATHER_API_KEY;
-// variable api_key ahora tiene el valor configurado
 
-// https://www.visualcrossing.com/weather/weather-data-services/Madrid?v=api
-//Madrid?unitGroup=us&include=current&key=9WQCPNN5TRJT9U6A6NR8TMFT8&contentType=json
 const baseUrl =
   "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
 
